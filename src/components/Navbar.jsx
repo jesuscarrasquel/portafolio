8447f2ae8a300dc3ff894dac445462643985d20d
Jesus Carrasquel <jesusmcc1106@gmail.com>
2021-11-23 18:01:04 +0100

    cuarto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                     <Img src={LogoPortafolio}/>
                 </Left>
                 <Right>
-                    <Anchor href={CV} target="_blank">Hoja de vida</Anchor>
+                    <Anchor href={CV} target="_blank">Curriculum</Anchor>
                 </Right>
             </Wrapper>
         </Container>
